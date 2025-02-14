d439e2879ad1089a3a46526a1cb9182e70599d8f
jeongho <jhjhjh2156@gmail.com>
2025-02-14 09:57:33 +0900

    0214_3

---
+++ frontend/src/app/components/chat/AIMessage.tsx
@@ -0,0 +1,46 @@
+import styles from '@/app/styles/chat.module.css';
+import { Keywords } from './Keywords';
+import { SuggestedQuestions } from './SuggestedQuestions';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from "rehype-raw";
+
+interface AIMessageProps {
+  message: string;
+  keywords: string[];
+  suggestedQuestions: string[];
+  onQuestionClick: (question: string) => void;
+  onKeywordClick: (keyword: string) => void;
+  isGenerating?: boolean;
+}
+
+export function AIMessage({ 
+  message, 
+  keywords, 
+  suggestedQuestions, 
+  onQuestionClick,
+  onKeywordClick,
+  isGenerating = false
+}: AIMessageProps) {
+  return (
+    <div className={styles.aiMessage}>
+      <div className={styles["markdown-body"]}>
+      <ReactMarkdown
+        >
+          {message}
+        </ReactMarkdown>
+        {/* {message} */}
+        {isGenerating && <span className={styles.cursor}>|</span>}
+      </div>
+      {!isGenerating && keywords.length > 0 && (
+        <>
+          <hr className={styles.messageDivider} />
+          <Keywords keywords={keywords} onKeywordClick={onKeywordClick} />
+          <SuggestedQuestions 
+            questions={suggestedQuestions}
+            onQuestionClick={onQuestionClick}
+          />
+        </>
+      )}
+    </div>
+  );
+}
